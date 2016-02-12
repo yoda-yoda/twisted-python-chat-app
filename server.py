2dad346dc2a54b24b827e313beec34d286585077
Master Yoda <deebeat@students.uonbi.ac.ke>
2016-02-12 03:37:30 +0300

    Show the number of clients connected / disconnected

--- server.py
+++ server.py
@@ -5,38 +5,44 @@ import time
 
 
 def timer():
-    return "[" + time.strftime("%H:%M:%S") + "] "
+    """Tmier function."""
+    return "[" + time.strftime("%Y-%m-%d %H:%M:%S") + "] "
 
 
 class EchoProtocol(basic.LineReceiver):
-    name = "Unnamed"
+    """Protocol class."""
 
     def connectionMade(self):
+        """Called when a connecection is made."""
         self.transport.write("You are now live\r\n")
-        self.sendLine("Enter A name Below...")
-        self.sendLine("")
-        print(dir(self.factory))
-        print(self.factory.clients)
+        self.sendLine("Enter A name: ")
         self.count = 0
         self.factory.clients.append(self)
-        self.host_name = self.transport.getPeer().host
+        self.host_name = self.transport.getHost().host
         print("{0}: New Connection from {1}".format(timer(), self.host_name))
+        print("{0} clients connected".format(len(self.factory.clients)))
 
     def connectionLost(self, reason):
+        """Called when a connection is lost."""
         self.sendMsg("- {} left.".format(self.host_name))
         print("{0} : {1} has lost connection".format(timer(), self.host_name))
-        print("Reason: {0}".format(dir(reason)))
+        print("Reason: {0}".format(reason.getErrorMessage()))
         self.factory.clients.remove(self)
+        print("{0} clients connected".format(len(self.factory.clients)))
 
     def dataReceived(self, data):
+        """Called when data is streame."""
         self.sendMsg(data)
 
     def sendMsg(self, message):
+        """Send messages."""
         for client in self.factory.clients:
             client.transport.write(message + "\n")
 
 
 class EchoServerFactory(protocol.ClientFactory):
+    """Server factory."""
+
     protocol = EchoProtocol
     clients = []
 
